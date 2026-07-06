05e39d3984a2af74999659b4facd5a66551449ee
RJ-885 <rtjohnson@uncg.edu>
2026-07-06 10:48:41 -0400

    Changes to be committed: 	new file:   backend-api/.gitattributes 	new file:   backend-api/.gitignore 	new file:   backend-api/.mvn/wrapper/maven-wrapper.properties 	new file:   backend-api/mvnw 	new file:   backend-api/mvnw.cmd 	new file:   backend-api/pom.xml 	new file:   backend-api/src/main/java/com/csc340/homefix_now/HomefixNowApplication.java 	new file:   backend-api/src/main/resources/application.properties 	new file:   backend-api/src/test/java/com/csc340/homefix_now/HomefixNowApplicationTests.java
     Changes not staged for commit:
    	modified:   backend-api/src/main/resources/application.properties

---
+++ backend-api/src/main/java/com/csc340/homefix_now/HomefixNowApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.homefix_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomefixNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomefixNowApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/csc340/homefix_now/HomefixNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc340.homefix_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomefixNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
