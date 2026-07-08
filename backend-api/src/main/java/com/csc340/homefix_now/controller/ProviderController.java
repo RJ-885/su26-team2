9cc428b3def321123f5ca7305b0145d9dfb58cee
Jacob <Jacob.strider0@gmail.com>
2026-07-08 00:06:35 -0400

    Update provider backend

--- backend-api/src/main/java/com/csc340/homefix_now/controller/ProviderController.java
+++ backend-api/src/main/java/com/csc340/homefix_now/controller/ProviderController.java
@@ -1,5 +1,19 @@
 package com.csc340.homefix_now.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.csc340.homefix_now.service.ProviderService;
+
+@RestController
+@RequestMapping("/api/providers")
+
 public class ProviderController {
     
+    private final ProviderService providerService;
+
+    public ProviderController(ProviderService providerService) {
+        this.providerService = providerService;
+    }
+
 }
